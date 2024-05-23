45db71acb39c23cf11d4012313d66f18a2d297dd
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-05-23 22:16:57 +0300

    Добавьте файлы проекта.

---
+++ AsyncClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace AsyncClient
+{
+    internal class Program
+    {
+        static async Task Main (string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+            try
+            {
+                Console.WriteLine("Client has started");
+                await socket.ConnectAsync(endPoint);
+                Console.WriteLine("Напишіть 'DATA'  щоб отримати дату, або 'TIME' щоб отримати час.");
+                string message = Console.ReadLine();
+                await socket.SendAsync(Encoding.UTF8.GetBytes(message),SocketFlags.None);
+                var buf = new byte[1024];
+                int bytesRead = await socket.ReceiveAsync(buf);
+                string responseMessage = Encoding.UTF8.GetString(buf, 0, bytesRead);
+                Console.WriteLine($"Отримано від {socket?.RemoteEndPoint?.ToString()}: {responseMessage}");
+                socket?.Shutdown(SocketShutdown.Both);
+                socket?.Close();
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            Console.WriteLine("\nНатисни будь яку кнопку щоб вийти....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AsyncServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace AsyncServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Any, 1234);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            Console.WriteLine("Server has started");
+
+
+            while (true)
+            {
+                var client = await socket.AcceptAsync();
+                _ = Task.Run(() => HandleClientAsync(client));
+            }
+        }
+
+        private static async void HandleClientAsync(Socket client)
+        {
+            var buf = new byte[1024];
+            int bytesRead = await client.ReceiveAsync(buf, SocketFlags.None);
+            string receivedMessage = Encoding.UTF8.GetString(buf, 0, bytesRead).Trim();
+            Console.WriteLine($"Отримано від  {client?.RemoteEndPoint?.ToString()}: {receivedMessage}");
+
+            string responseMessage;
+            if (receivedMessage.Trim().ToUpper() == "DATA")
+            {
+                responseMessage = DateTime.Now.ToString("yyy-MM-dd");
+            }
+            else if (receivedMessage.Trim().ToUpper() == "TIME")
+            {
+                responseMessage = DateTime.Now.ToString("HH:mm:ss");
+            }
+            else
+            {
+                responseMessage = "Invalid request";
+            }
+           await client?.SendAsync(Encoding.UTF8.GetBytes(responseMessage), SocketFlags.None);
+            client?.Shutdown(SocketShutdown.Both);
+            client?.Close();
+
+        }
+    }
+}
+
